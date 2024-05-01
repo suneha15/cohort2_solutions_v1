2a9a8c35a6f17dcc1ee793449fa441e10c064297
suneha11 <suneha.ss1502@gmail.com>
2024-05-01 20:18:32 +0530

    removed comma

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -4,7 +4,7 @@ function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
 
-    const username = req.headers.username,
+    const username = req.headers.username
     const password = req.headers.password
 
     User.findOne({
