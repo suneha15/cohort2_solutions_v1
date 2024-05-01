e136d58e070b7098f06de8602d9f4b9d60ff8ccf
suneha11 <suneha.ss1502@gmail.com>
2024-05-02 02:56:30 +0530

    fetching purchased courses by a user

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -48,8 +48,23 @@ router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+    const user = await  User.findOne({
+        username : req.headers.username
+    });
+
+    console.log(user.purchasedCourses);
+
+    const courses = await Course.find({
+        _id : {
+            "$in" : user.purchasedCourses
+        }
+    });
+
+    res.json({
+        courses : courses
+    })
 });
 
 module.exports = router
