6aabee9f259597740cec0831d1b1016050a38fe2
suneha11 <suneha.ss1502@gmail.com>
2024-05-01 19:29:30 +0530

    defining schemas

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://suneha15:python%40123@cluster0.qz2qdpw.mongodb.net/');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password  : String,
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password  : String,
+    purchasedCourses : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title : String,
+    description : String,
+    price : Number,
+    imageLink : String
+
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
