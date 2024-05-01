3daf99fd854ad58844503a948a8b19879cceab39
suneha11 <suneha.ss1502@gmail.com>
2024-05-02 01:27:32 +0530

    creating a new user

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,10 +1,22 @@
 const { Router } = require("express");
 const router = Router();
+const { User } = require("../db")
 const userMiddleware = require("../middleware/user");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    await User.create({
+        username : username,
+        password : password
+    })
+    res.json({
+        msg : "user created successfully"
+    })
+    
 });
 
 router.get('/courses', (req, res) => {
