0671757a047dbc36149a579441ddc6c3503e9028
suneha11 <suneha.ss1502@gmail.com>
2024-05-03 21:17:44 +0530

    implementing signin endpoint for admin using jwt

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,49 @@
 const { Router } = require("express");
+const jwt = require("jsonwebtoken");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
+const {JWT_SECRET} = require("../config")
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //check if a username with this username already exists
+    await Admin.create({
+        username : username,
+        password : password
+    })
+    res.json({
+        msg : "Admin created successfully!"
+    })
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+   
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const admin = await Admin.find({
+        username,
+        password
+    })
+    if (admin){
+        const token =  jwt.sign({
+            username
+        },JWT_SECRET);
+        res.json({
+            token
+        })
+    }else{
+        res.status(411).json({
+            message : "Incorrect emailid or password"
+        })
+    }
+  
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
