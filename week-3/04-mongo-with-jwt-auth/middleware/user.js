c02594bfe2fc9ed306018f63fa8d52bbd4f285cc
suneha11 <suneha.ss1502@gmail.com>
2024-05-03 16:10:10 +0530

    defining middleware for user authentication

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,25 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../index");
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtToken = words[1];
+    const decodedValue = jwt.verify(jwtToken,secret);
+
+    if(decodedValue.username){
+        next();
+    }else{
+        res.status(403).json({
+            msg : "You are not authenticated!"
+        })
+    }
 }
 
 module.exports = userMiddleware;
+
+//when you're signing the jwt in the signin endpoint you should mention - username , type : admin || "user"
+
+//authentication , authorization -> eventhough the username can be same but the authorization they have might be different
+
+//eg : (decodedValue.username && decodedValue.type = 'user')
