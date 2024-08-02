7c957cd6837ced9848bb636c0aee14fea22758d5
suneha11 <suneha.ss1502@gmail.com>
2024-08-02 17:47:58 +0530

    using-useNavigate-for-client-side-routing

--- week-7/class-7.1/react-routes/src/App.jsx
+++ week-7/class-7.1/react-routes/src/App.jsx
@@ -2,31 +2,45 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import { Dashboard } from './components/Dashboard'
 import { Landing } from './components/Landing'
-
 
 function App() {
 
   return (
     <div>
+     <BrowserRouter>
+      <Appbar />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/" element={<Landing/>}/>
+      </Routes>
+     </BrowserRouter>
+    </div>
+  )
+}
+
+function Appbar(){
+  const navigate = useNavigate();
+
+  return(
+    <div>
       <div>
         <button onClick={()=>{
-          window.location.href = "/"; 
+          navigate("/");
+
+          //window.location.href = "/"; 
           //this is not the right way to go from one page to another, if you're using client side routing
           //reloading happens in the network when we switch buttons
         }}>Landing Page</button>
+
         <button onClick={()=>{
-          window.location.href = "/dashboard";
+          navigate("/dashboard");
+          //window.location.href = "/dashboard";
         }}>Dashboard</button>
+
       </div>
-     <BrowserRouter>
-      <Routes>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/' element={<Landing/>}/>
-      </Routes>
-     </BrowserRouter>
     </div>
   )
 }
