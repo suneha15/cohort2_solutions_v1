ef3b3e8e84f8ebd379fb7a4ba7c56e1879056264
suneha11 <suneha.ss1502@gmail.com>
2024-08-02 23:52:43 +0530

    routing

--- week-7/class-7.1/react-routes/src/App.jsx
+++ week-7/class-7.1/react-routes/src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { lazy,Suspense } from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css'
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+
+const Dashboard = lazy(() => import('./components/Dashboard'))
+const Landing = lazy(() => import('./components/Landing'))
 
 function App() {
+  
+  //suspense API 
+  /*another API that react provides you which is used for cases like -
+			-asynchronous component fetching
+			-asynchronous data fetching 
+  */
 
   return (
     <div>
      <BrowserRouter>
       <Appbar />
       <Routes>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/" element={<Landing/>}/>
-      </Routes>
+        <Route path="/dashboard" element={
+          <Suspense fallback={"loading..."}>
+            <Dashboard/>
+          </Suspense>}/>
+        <Route path="/" element={
+          <Suspense fallback={"loading.."}>
+            <Landing/>
+          </Suspense>}/>
+        </Routes>
      </BrowserRouter>
     </div>
   )
