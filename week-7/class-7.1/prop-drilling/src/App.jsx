2482a7a629d1ea468c42f6730a604353b6ca1e0e
suneha11 <suneha.ss1502@gmail.com>
2024-08-03 13:20:01 +0530

    prop-drilling-1

--- week-7/class-7.1/prop-drilling/src/App.jsx
+++ week-7/class-7.1/prop-drilling/src/App.jsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 import './App.css'
 
+//the goal is to create an application where someone can increase or decrease a global count that gets rendered in this <Count/> component.
+
 function App() {
+  const [count,setCount] = useState(0);
 
   return (
     <div>
-      
+      <Count counting={count}/>
+      <Buttons count = {count} setCount = {setCount} />
     </div>
   )
+}
+
+function Count({counting}){
+  return <div>
+    {counting}
+  </div>
+}
+
+function Buttons({count,setCount}){
+  return <div>
+    <button onClick={()=>{
+        setCount(count +1)
+    }}>Increase</button>
+    <button onClick={()=>{
+        setCount(count -1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
