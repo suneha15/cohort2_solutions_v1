6ba477f54e3928bbb99d3b569808e8c33fe8bd24
suneha11 <suneha.ss1502@gmail.com>
2024-08-28 12:46:29 +0530

    component

--- week-8/class-8.1/tailwind-project/src/App.jsx
+++ week-8/class-8.1/tailwind-project/src/App.jsx
@@ -1,29 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import { GridandFlex } from './components/GridandFlex'
+import { RevenueCard } from './components/RevenueCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div style={{display: "flex" , justifyContent:"center"}}>
-        <div style={{backgroundColor: "lightcoral"}}>Hi</div>
-        <div style={{backgroundColor: "lightgreen"}}>Hi</div>
-        <div style={{backgroundColor: "lightpink"}}>Hi</div>
-      </div>
-      <br /><br />
-
-      <div className='flex justify-between'>
-          <div className='bg-red-500'>Hello</div>
-          <div className='bg-yellow-500'>Hello</div>
-          <div className='bg-sky-500/75'>Hello</div>
-      </div>
-    <br /><br />
-
-      <div className='grid grid-cols-10'>
-        <div className='bg-red-100 col-span-5'>Hey there from 1st div</div>
-        <div className='bg-yellow-100 col-span-3'>Hey threre from 2nd div</div>
-        <div className='bg-green-100 col-span-2'>Hey there from 3rd div</div>
+      <div className='grid grid-cols-3'>
+        <RevenueCard title={"Amount Pending"} amount={"92,312.20"} orderCount={13}/>
       </div>
     </>
   )
