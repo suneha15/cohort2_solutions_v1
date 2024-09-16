d397781223726abd4762b5ad4bd29009686f9e30
suneha11 <suneha.ss1502@gmail.com>
2024-09-16 06:02:00 +0530

    middlewares

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -10,14 +10,6 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-
-//error handling middleware
-
-app.use(function errorCount(err,req,res,next){
-  res.status(404).send({})
-    errorCount = errorCount + 1;
-})
-
 app.get('/user', function(req, res) {
   throw new Error("User not found");
   res.status(200).json({ name: 'john' });
@@ -30,5 +22,12 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+//error handling middleware
+
+app.use(function errorCount(err,req,res,next){
+  res.status(404).send({})
+    errorCount = errorCount + 1;
+})
 
 module.exports = app;
