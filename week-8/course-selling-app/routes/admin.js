cb628d4df291ca8ecd6726f613fca052116764e9
suneha11 <suneha.ss1502@gmail.com>
2024-09-30 03:35:49 +0530

    add-auth-endpoints

--- week-8/course-selling-app/routes/admin.js
+++ week-8/course-selling-app/routes/admin.js
@@ -1,18 +1,49 @@
 const express = require("express");
 const adminModel = require("../root/db");
 const {Router} = require("express");
+const {jwt} = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = "56789"
+
 const adminRouter = Router();
 
-adminRouter.post("/signup",(req,res)=>{
+//zod, jwt, bcrypt
+
+adminRouter.post("/signup", async  (req,res)=>{
+    const { email, password, firstname, lastname} = req.body;
+
+    await adminModel.create({
+        email: email,
+        password: password,
+        firstname: firstname,
+        lastname: lastname
+    })
+
     res.json({
         message : "Signup endpoint"
     })
 })
 
-adminRouter.post("/signin",(req,res)=>{
-    res.json({
-        message : "signin endpoint"
+adminRouter.post("/signin",async (req,res)=>{
+    const { email, password} = req.body;
+
+    const admin = await adminModel.findOne({
+        email: email,
+        password: password
     })
+
+    if(admin){
+        const token = jwt.sign({
+            id : admin._id 
+        }, JWT_ADMIN_PASSWORD)
+
+        res.json({
+            token: token
+        })
+    } else{
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 })
 
 adminRouter.post("/createCourse",(req,res)=>{
@@ -27,7 +58,7 @@ adminRouter.put("/createCourse",(req,res)=>{
     })
 })
 
-adminRouter.get("/createCourse",(req,res)=>{
+adminRouter.get("/course/bulk",(req,res)=>{
     res.json({
         message : "give me all the course that ive created"
     })
--- week-8/course-selling-app/routes/user.js
+++ week-8/course-selling-app/routes/user.js
@@ -1,17 +1,58 @@
 const express = require("express");
 const {Router} = require("express");
+const {userModel} = require("../db")
+const {jwt} = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "12345"
+
 const userRouter = Router(); //its a built in middleware function in express
 
-userRouter.post("/signup",(req,res)=>{
+userRouter.post("/signup",async (req,res)=>{
+    const { email, password, firstName, lastName } = req.body;
+    
+    //todo : adding zod validation
+    //todo : hash the password so plaintext password is not stored in the DB
+    //todo : put inside try catch block
+    try{
+        await userModel.create({
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastname
+        })
+    }catch(e){
+      console.log(e) 
+    }
+
     res.json({
-        message : "Signup endpoint"
+        message : "Signup succeeded"
     })
 })
 
-userRouter.post("/signin",(req,res)=>{
-    res.json({
-        message : "signin endpoint"
-    })
+userRouter.post("/signin",async (req,res)=>{
+    const { email, password } = req.body;
+
+    //todo : ideally password should be hashed ,and hence you cannot compare the user provided password and the database password
+
+    const user = await userModel.findOne({
+        email: email,
+        password: password
+    });
+
+    if(user){
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_USER_PASSWORD)
+
+        //Do cookie logic
+
+        res.json({
+            token: token
+        })
+    }else{
+        res.status(403).json({
+            message : "incorrect credentials"
+        })
+    }
 })
 
 userRouter.get("/purchases",(req,res)=>{
